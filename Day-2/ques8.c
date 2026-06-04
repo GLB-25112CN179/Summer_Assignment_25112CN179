dfa1b88b6cd7ecc339771f5dc27e4fb7554e53cd
SARTHAK JAIN <286733112+GLB-25112CN179@users.noreply.github.com>
2026-06-04 21:10:12 +0530

    Add files via upload

---
+++ Day-2/ques8.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() 
+{
+    int num, originalNum, reverse = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (num != 0) 
+{
+        remainder = num % 10;
+        reverse = reverse * 10 + remainder;
+        num = num / 10;
+    }
+
+    if (originalNum == reverse)
+        printf("%d is a Palindrome Number.\n", originalNum);
+    else
+        printf("%d is not a Palindrome Number.\n", originalNum);
+
+    return 0;
+}
